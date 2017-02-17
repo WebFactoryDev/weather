b49eebe986f6ec7360294263c6e3edb39711c95a
WebFactoryDev <roberto.martinez@webfactory.mx>
2017-02-17 15:11:00 -0600

    cambio app para base

--- config/app.php
+++ config/app.php
@@ -229,8 +229,8 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'root',
-            'password' => '',
+            'username' => 'Wf_clima',
+            'password' => 'hs7W4s*0',
             'database' => 'weather',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
